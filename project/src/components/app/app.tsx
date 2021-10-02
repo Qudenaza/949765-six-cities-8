5f9821b6f4b6f58fb35b28c1032eef51d7d89f47
Qudenaza <qudenaza@gmail.com>
2021-10-02 10:02:40 +0300

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,17 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Header from '../header/header';
+import Main from '../main/main';
+
+type Props = {
+  rentCount: number,
+};
+
+function App({rentCount}: Props): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <Header />
+      <Main rentCount={rentCount}/>
+    </div>
+  );
 }
 
 export default App;
